f4cf199b307820db9a0103958638a1bd30aca9fa
Ivan Duka <ivan.duka.pm@gmail.com>
2021-02-23 13:17:51 -0800

    fail!

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"os"
 )
 
 func main() {
-	port := 3333
-	resp, err := http.Get(fmt.Sprintf("http://localhost:%v", port))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		fmt.Println("ERROR", resp.StatusCode)
-		os.Exit(1)
-	}
-
-	fmt.Println("OK", resp.StatusCode)
-	os.Exit(0)
+	os.Exit(1)
+	//port := 3333
+	//resp, err := http.Get(fmt.Sprintf("http://localhost:%v", port))
+	//if err != nil {
+	//	fmt.Println(err)
+	//	os.Exit(1)
+	//}
+	//
+	//if resp.StatusCode < 200 || resp.StatusCode > 299 {
+	//	fmt.Println("ERROR", resp.StatusCode)
+	//	os.Exit(1)
+	//}
+	//
+	//fmt.Println("OK", resp.StatusCode)
+	//os.Exit(0)
 }
